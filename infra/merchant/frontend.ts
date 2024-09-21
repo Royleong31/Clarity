5f67f274760eb730142d96f245ef2dd52d3d2266
Roy Leong <l3ongroy@gmail.com>
2024-09-21 22:14:29 +0800

    add envs

--- infra/merchant/frontend.ts
+++ infra/merchant/frontend.ts
@@ -1,9 +1,36 @@
 /// <reference path="../.sst/platform/config.d.ts" />
+import "dotenv/config";
 
 import { api as clarityApi } from "../clarity/api";
 import { api as merchantApi } from "./api";
 
 const region = aws.getRegionOutput().name;
+
+const VITE_DYNAMIC_ENV_ID = process.env.VITE_DYNAMIC_ENV_ID;
+const VITE_PAYMASTER_KEY = process.env.VITE_PAYMASTER_KEY;
+const VITE_BUNDLER_KEY = process.env.VITE_BUNDLER_KEY;
+const VITE_1INCH_KEY = process.env.VITE_1INCH_KEY;
+const VITE_WID = process.env.VITE_WID;
+const VITE_1INCH_URL = process.env.VITE_1INCH_URL;
+
+if (!VITE_DYNAMIC_ENV_ID) {
+  throw new Error("VITE_DYNAMIC_ENV_ID is required");
+}
+if (!VITE_PAYMASTER_KEY) {
+  throw new Error("VITE_PAYMASTER_KEY is required");
+}
+if (!VITE_BUNDLER_KEY) {
+  throw new Error("VITE_BUNDLER_KEY is required");
+}
+if (!VITE_1INCH_KEY) {
+  throw new Error("VITE_1INCH_KEY is required");
+}
+if (!VITE_WID) {
+  throw new Error("VITE_WID is required");
+}
+if (!VITE_1INCH_URL) {
+  throw new Error("VITE_1INCH_URL is required");
+}
 
 export const frontend = new sst.aws.StaticSite("MerchantFrontend", {
   path: "packages/merchant-frontend",
@@ -16,5 +43,11 @@ export const frontend = new sst.aws.StaticSite("MerchantFrontend", {
     VITE_PUBLIC_REGION: region,
     VITE_PUBLIC_CLARITY_API_URL: clarityApi.url,
     VITE_PUBLIC_MERCHANT_API_URL: merchantApi.url,
+    VITE_DYNAMIC_ENV_ID,
+    VITE_PAYMASTER_KEY,
+    VITE_BUNDLER_KEY,
+    VITE_1INCH_KEY,
+    VITE_WID,
+    VITE_1INCH_URL,
   },
 });
--- packages/merchant-frontend/src/sst-env.d.ts
+++ packages/merchant-frontend/src/sst-env.d.ts
@@ -3,6 +3,12 @@
     readonly VITE_PUBLIC_REGION: string
   readonly VITE_PUBLIC_CLARITY_API_URL: string
   readonly VITE_PUBLIC_MERCHANT_API_URL: string
+  readonly VITE_DYNAMIC_ENV_ID: string
+  readonly VITE_PAYMASTER_KEY: string
+  readonly VITE_BUNDLER_KEY: string
+  readonly VITE_1INCH_KEY: string
+  readonly VITE_WID: string
+  readonly VITE_1INCH_URL: string
   }
   interface ImportMeta {
     readonly env: ImportMetaEnv
